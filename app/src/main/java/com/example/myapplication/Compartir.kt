2b277055326ae8951950ed9724e7145c3b4d55cf
martarc2001 <martarc2001@hotmail.com>
2022-11-05 12:52:13 +0100

    Borra texto si le das a cancelar

---
+++ app/src/main/java/com/example/myapplication/Compartir.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+import android.widget.Button
+import androidx.compose.ui.platform.LocalContext
+
+fun Compartir(){
+/*
+    Button(
+        onShareClick = {
+
+            val context = LocalContext.current
+
+            val type = "text/plain"
+            val subject = "Your subject"
+            val extraText = "https://www.google.com/codes/${variable.id}"
+            val shareWith = "ShareWith"
+
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = type
+            intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            intent.putExtra(Intent.EXTRA_TEXT, extraText)
+
+            ContextCompat.startActivity(
+                context,
+                Intent.createChooser(intent, shareWith),
+                null
+            )
+        })*/
+}
--- app/src/main/java/com/example/myapplication/Dialog.kt
+++ app/src/main/java/com/example/myapplication/Dialog.kt
@@ -1,24 +1,30 @@
 package com.example.myapplication
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.DialogProperties
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun dialog(){
     var popup by remember { mutableStateOf(false) }
-    var text by remember { mutableStateOf("")}
+    var textoFinal by remember { mutableStateOf(TextFieldValue())}
 
     Box(
         contentAlignment = Alignment.Center
@@ -46,25 +52,61 @@ fun dialog(){
 
 
             if(popup){
-                AlertDialog(
 
+                AlertDialog(
+                    properties = DialogProperties(usePlatformDefaultWidth = false),
+                    modifier=Modifier.wrapContentHeight(),
                     onDismissRequest = {popup = false},
                     title = { Text(text = "Add")},
                     text = {
                         Column() {
+                            val maxChar=50
                             TextField(
-                                value = text,
-                                onValueChange = {text = it}
+                                value = textoFinal,
+                                onValueChange = { if (it.text.length <= maxChar) textoFinal = it },
+                                textStyle = LocalTextStyle.current.copy(
+                                    color = MaterialTheme.colorScheme.onSurface,
+                                    fontSize = 30.sp,
+                                    lineHeight=30.sp //Interlineado
+                                ),
+                                //lineHeight=30.sp,
+                                trailingIcon = {
+                                    Icon(Icons.Default.Clear,
+                                        contentDescription = "clear text",
+                                        modifier = Modifier
+                                            .clickable {
+                                                textoFinal=TextFieldValue("")
+                                            }
+                                    )
+                                },
+                                colors = TextFieldDefaults.textFieldColors(
+                                    backgroundColor = Color.Transparent
+                                )
                             )
+                            Text(
+                                text = "${textoFinal.text.length} / $maxChar",
+                                textAlign = TextAlign.End,
+                                modifier = Modifier.fillMaxWidth().padding(end = 16.dp)
+                            )
+                            //Text("The textfield has this text: " + textoFinal)
+
                         }
                     },
                     confirmButton = {
                         Row(
-                            modifier = Modifier.padding(all = 8.dp)
+                            //modifier = Modifier.padding(all = 8.dp)
                         ) {
-                            Button(onClick = {popup = false}) {
-
+                            TextButton(onClick = {popup = false}) {Text("Confirm")}
+                        }
+                    },
+                    dismissButton = {
+                        TextButton(
+                            onClick = {
+                                textoFinal=TextFieldValue("")
+                                popup = false
                             }
+                        ) {
+                            Text("Dismiss")
                         }
                     }
                 )
